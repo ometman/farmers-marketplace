4a7de1f5d3697b0180946f51e606abcd30b472ee
ometman <revomet@gmail.com>
2025-01-28 03:21:29 +0000

    Install express-rate-limit, configure limiter

--- bkend/src/app.js
+++ bkend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit')
 
 const app = express();
 
@@ -11,6 +12,16 @@ app.use(cors({
     origin: process.env.FRONT_URL || 'http://localhost:3000',
     credentials: true,
 }));
+
+// rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15mins
+    max: 100, // 100 requests per window
+    message: 'Too many requests from this IP, Please try again later in 15mins',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter);
 
 app.use(morgan('dev')); // output error coloring
 app.use(express.json()); // middleware parsing only json, matching content-type with header

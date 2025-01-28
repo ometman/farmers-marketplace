996ce3ab064784aee21ef4b13c5ea2751159d58c
ometman <revomet@gmail.com>
2025-01-28 03:11:31 +0000

    Update cors in app.js - Pointing frontend

--- bkend/src/app.js
+++ bkend/src/app.js
@@ -5,10 +5,14 @@ const morgan = require('morgan');
 
 const app = express();
 
-// middlewares
-app.use(cors());
-app.use(helmet());
-app.use(morgan('dev'));
-app.use(express.json());
+// middlewares - security
+app.use(helmet()); // security headers
+app.use(cors({
+    origin: process.env.FRONT_URL || 'http://localhost:3000',
+    credentials: true,
+}));
+
+app.use(morgan('dev')); // output error coloring
+app.use(express.json()); // middleware parsing only json, matching content-type with header
 
 module.exports = app;
